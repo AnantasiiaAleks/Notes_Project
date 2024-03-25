58ee95fac89fefd05beb3b50b4c3330690554b6c
AnastasiiaAleks <chiffka@gmail.com>
2024-03-25 10:18:40 +0300

    Added class Note

--- Note.py
+++ Note.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 
 class Note:
-    def __init__(self, id = str(uuid.uuid4().int)[:4],
-                 date = f'{datetime.now:%d.%m.%Y %H:%M:%S}',
-                 title = 'title',
-                 text = 'text'):
+    def __init__(self, id=str(uuid.uuid4().int)[:4],
+                 date=f'{datetime.now:%d.%m.%Y %H:%M:%S}',
+                 title='title',
+                 text='text'):
         self.id = id
         self.date = date
         self.title = title
@@ -14,10 +14,13 @@ class Note:
 
     def get_id(self):
         return self.id
+
     def get_date(self):
         return self.date
+
     def get_title(self):
         return self.title
+
     def get_text(self):
         return self.text
 
@@ -31,7 +34,7 @@ class Note:
         self.title = title
 
     def set_text(self, text):
-        self.text=text
+        self.text = text
 
     def to_string(self):
         return self.id + ';' + self.title + ';' + self.text + ';' + self.date
